f264581830b9ea25559579f153267e5051519a60
Mark Athanason <mark.athanason@vumc.org>
2019-08-10 09:39:28 -0500

    homework

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start_date><br/>"
-        f"/api/v1.0/<start_date>/<end_date><br/>")
+        f"/api/v1.0/start_date<br/>"
+        f"/api/v1.0/start_date/end_date<br/>")
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
